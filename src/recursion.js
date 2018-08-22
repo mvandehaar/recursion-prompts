61f29062fbd419e566ef72b66eedd63fc855e222
Mitchell Vandehaar <mitchvandehaar@gmail.com>
2018-08-21 18:23:06 -0700

    completed up to no.8 power of two

--- src/recursion.js
+++ src/recursion.js
@@ -74,26 +74,54 @@ var sumBelow = function(n) {
     }
   }
 };
-console.log(sumBelow(10)); // 45
-console.log(sumBelow(-1)); // 21
+// console.log(sumBelow(10)); // 45
+// console.log(sumBelow(-1)); // 21
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
-  var arr = [];
-  // var arr = Array.from({length: (y-x-1)})
-  if (arr.length === y-x-1) {
-    return arr
+  if (y === x) {
+    return [];
   }
+    if (x > y) {
+      if (x - y === 1) {
+        return []
+      } else {
+        var list = range(x, y + 1)
+        list.push(y + 1)
+        // console.log(list)
+        return list
+      }
+    }
+    // console.log('x', x)
+     
+    if (y - x === 1) {
+      return []
+    } else {
+      var list = range(x, y - 1);
+      list.push(y - 1);
+      return list;
+    }
+
+
 };
-// console.log(range(2,9))
+// console.log(range(7, 2))
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
-};
 
+  if (exp === 0) {
+    return 1;
+  } else if (exp < 0) {
+    return 1 / base * exponent(base, exp + 1);
+  } else if (exp > 0) {
+    return base * exponent(base, exp - 1);
+  }
+
+};
+console.log(exponent(5,-4)); // 64)
 // 8. Determine if a number is a power of two.
 // powerOfTwo(1); // true
 // powerOfTwo(16); // true
